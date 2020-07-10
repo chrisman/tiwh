15b66d2ba2aa689f8990b1addbe407e5419eec51
Christopher P. Brown <christopher.p.brown@gmail.com>
2020-07-10 14:59:10 -0600

    create searches model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_204124) do
+ActiveRecord::Schema.define(version: 2020_07_10_205040) do
 
   create_table "locations", force: :cascade do |t|
     t.integer "woeid"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.integer "location_id"
+    t.text "result"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
