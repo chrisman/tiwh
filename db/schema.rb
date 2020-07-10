86b16ec68c96df164876ccb391cd1183fd7fecde
Christopher P. Brown <christopher.p.brown@gmail.com>
2020-07-10 17:05:11 -0600

    migrate from sqlite3 to postgresql
    forgot that sqlite doesn't work on heroku 😭

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_07_10_205040) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "locations", force: :cascade do |t|
     t.integer "woeid"
     t.text "description"
